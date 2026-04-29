0e16bee2998681790aed404c121e5df4bc5df463
smallkirby <ssmallkirby@gmail.com>
2026-04-29 12:58:41 +0900

    feat(aarch64:psci): init PSCI
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- urthr/arch/aarch64/asm.zig
+++ urthr/arch/aarch64/asm.zig
@@ -63,6 +63,34 @@ pub fn modifySreg(comptime reg: SystemReg, value: anytype) void {
     msr(reg, now);
 }
 
+pub fn smc(arg0: u64, arg1: u64, arg2: u64, arg3: u64) u64 {
+    return asm volatile (
+        \\mov x0, %[x0]
+        \\mov x1, %[x1]
+        \\mov x2, %[x2]
+        \\mov x3, %[x3]
+        \\smc #0
+        : [ret] "={x0}" (-> u64),
+        : [x0] "r" (arg0),
+          [x1] "r" (arg1),
+          [x2] "r" (arg2),
+          [x3] "r" (arg3),
+        : .{
+          .x4 = true,
+          .x5 = true,
+          .x6 = true,
+          .x7 = true,
+          .x8 = true,
+          .x9 = true,
+          .x10 = true,
+          .x11 = true,
+          .x12 = true,
+          .x13 = true,
+          .x14 = true,
+          .memory = true,
+        });
+}
+
 pub fn wfe() void {
     asm volatile ("wfe");
 }
