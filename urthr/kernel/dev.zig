9d047e6ba5df6fba7b37074460688d7abe39dd3c
smallkirby <ssmallkirby@gmail.com>
2026-05-06 18:35:39 +0900

    refactor(devfs): refactor devfs initialization into dedicated function
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- urthr/kernel/dev.zig
+++ urthr/kernel/dev.zig
@@ -1,1 +1,29 @@
-pub const console = @import("dev/console.zig");
+//! Devices under /dev filesystem.
+
+const console = @import("dev/console.zig");
+
+/// Initialize devfs.
+pub fn init() fs.Error!void {
+    const allocator = urd.mem.getGeneralAllocator();
+    const cur = sched.getCurrent();
+    rtt.expectEqual(0, cur.id);
+
+    // Mount devfs at /dev.
+    const devfs = try fs.DevFs.init(allocator);
+    const mntpnt = try fs.resolve("/dev", allocator);
+    defer mntpnt.dentry.unref();
+    try fs.mount(mntpnt, devfs.filesystem(), allocator);
+
+    // Register devices.
+    try devfs.registerDevice("console", console.file_ops);
+}
+
+// =============================================================
+// Imports
+// =============================================================
+
+const common = @import("common");
+const rtt = common.rtt;
+const urd = @import("urthr");
+const fs = urd.fs;
+const sched = @import("sched.zig");
