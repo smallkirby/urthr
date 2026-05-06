a191019a749adc214f207cbc0977d359e4ce3014
smallkirby <ssmallkirby@gmail.com>
2026-05-06 12:49:28 +0900

    feat(task): minimal PC/SP setup to enter userland
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- urthr/kernel/task.zig
+++ urthr/kernel/task.zig
@@ -3,3 +3,38 @@
 pub const loader = @import("task/loader.zig");
 pub const thread = @import("task/thread.zig");
 pub const Vmm = @import("task/Vmm.zig");
+
+/// The number of pages allocated for user stack.
+const num_stack_pages = 5;
+/// Base address of the user stack.
+const stack_base = 0x7FFF_FF00_0000;
+
+/// Enter userland by loading the specified executable.
+///
+/// This function does not return on success.
+pub fn enterUser(filename: []const u8) !noreturn {
+    const current = sched.getCurrent();
+
+    // Load the executable.
+    const entry = try loader.load(current, filename);
+
+    // Prepare user stack.
+    const stack = try current.vmm.map(
+        stack_base,
+        num_stack_pages * urd.mem.page_size,
+        .rw,
+    );
+    @memset(stack, 0);
+    const sp = stack_base + num_stack_pages * urd.mem.page_size;
+
+    // Enter userland.
+    arch.thread.enterUserland(entry, sp);
+}
+
+// =============================================================
+// Imports
+// =============================================================
+
+const arch = @import("arch").impl;
+const urd = @import("urthr");
+const sched = urd.sched;
