4562dff7ec55fc8743e69eaf502c326133dfb4db
smallkirby <ssmallkirby@gmail.com>
2026-05-10 17:55:41 +0900

    feat(posix): implement mprotect syscall
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- urthr/kernel/posix/task.zig
+++ urthr/kernel/posix/task.zig
@@ -22,11 +22,59 @@ pub fn sysBrk(addr: usize) ReturnType {
     return .success(@bitCast(new));
 }
 
+/// Memory protection flags.
+const Mprot = packed struct(u32) {
+    /// Readable.
+    read: bool,
+    /// Writable.
+    write: bool,
+    /// Executable.
+    exec: bool,
+    _3: u21 = 0,
+    /// Whether the mapping grows downwards.
+    growsdown: bool,
+    /// Whether the mapping grows upwards.
+    growsup: bool,
+    _26: u6 = 0,
+
+    fn permission(self: Mprot) Permission {
+        return .{
+            .ur = self.read,
+            .uw = self.write,
+            .ux = self.exec,
+            .kr = true,
+            .kw = self.write,
+            .kx = false,
+        };
+    }
+};
+
+/// System call: mprotect
+pub fn sysMprotect(addr: usize, len: usize, prot: Mprot) ReturnType {
+    if (addr % urd.mem.page_size != 0) return ReturnType.err(.inval);
+
+    const current = sched.getCurrent();
+    const aligned_len = std.mem.alignForward(usize, len, urd.mem.page_size);
+
+    if (aligned_len == 0) {
+        return .success(0);
+    }
+
+    current.vmm.remap(addr, aligned_len, prot.permission()) catch |e| switch (e) {
+        error.OutOfMemory => return ReturnType.err(.nomem),
+        else => return ReturnType.err(.inval),
+    };
+
+    return .success(0);
+}
+
 // =============================================================
 // Imports
 // =============================================================
 
 const std = @import("std");
+const common = @import("common");
+const Permission = common.mem.Permission;
 const urd = @import("urthr");
 const sched = urd.sched;
 const task = urd.task;
