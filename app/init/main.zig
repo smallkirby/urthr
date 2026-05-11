e054d6a0058fdc2aba265d05373ad43fd18ddc70
smallkirby <ssmallkirby@gmail.com>
2026-05-11 21:10:24 +0900

    feat(init): print stack trace in panic
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- app/init/main.zig
+++ app/init/main.zig
@@ -1,7 +1,7 @@
 //! Experimental init executable.
 
-pub fn main() noreturn {
-    std.posix.system.exit_group(0);
+pub fn main(_: std.process.Init) !void {
+    log.info("Initial process started.", .{});
 }
 
 // =============================================================
@@ -13,8 +13,50 @@ pub fn panic(msg: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
 
     log.err("PANIC: {s}", .{msg});
 
+    var it = StackIterator.init(@frameAddress());
+    var ix: usize = 0;
+    while (it.next()) |frame| : (ix += 1) {
+        log.err("  #{d:0>2}: 0x{X:0>16}", .{ ix, frame });
+    }
+
     std.posix.system.exit_group(99);
 }
+
+const StackIterator = struct {
+    const Self = @This();
+
+    fp: usize,
+
+    pub fn init(start_fp: usize) Self {
+        return .{ .fp = start_fp };
+    }
+
+    pub fn next(self: *Self) ?usize {
+        const fp_addr = self.fp + 0;
+        const ra_addr = self.fp + @sizeOf(usize);
+
+        if (!isFrameValid(fp_addr, ra_addr)) {
+            return null;
+        }
+
+        const fp_ptr: *const usize = @ptrFromInt(fp_addr);
+        const ret_ptr: *const usize = @ptrFromInt(ra_addr);
+
+        const fp = fp_ptr.*;
+        const ra = ret_ptr.*;
+
+        self.fp = fp;
+        return ra;
+    }
+
+    fn isFrameValid(fp: usize, ra: usize) bool {
+        return fp != 0 and
+            ra != 0 and
+            std.mem.isAligned(fp, @sizeOf(usize)) and
+            std.mem.isAligned(ra, @sizeOf(usize)) and
+            true;
+    }
+};
 
 // =============================================================
 // Imports
