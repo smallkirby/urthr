5c6fc5ec976212359a2505f96d4f45c3078c664f
smallkirby <ssmallkirby@gmail.com>
2026-05-25 20:05:53 +0900

    feat(dd/xhc): start xHC controller
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- urthr/dd/xhci/Xhc.zig
+++ urthr/dd/xhci/Xhc.zig
@@ -16,6 +16,12 @@ operational: regs.Operational,
 /// Runtime registers module.
 runtime: regs.Runtime,
 
+/// DCBAA.
+///
+/// Device Context pointed to by DCBAA entry is owned by the xHC.
+/// Software must not modify them.
+dcbaa: Dcbaa,
+
 /// Command Ring.
 cring: rings.Ring,
 /// Event Ring.
@@ -95,7 +101,6 @@ pub fn initPci(hc: pci.Host, addr: pci.DevAddr) Error!*Self {
 pub fn initMmio(base: usize) Error!*Self {
     const self = try mem.bin.create(Self);
     errdefer mem.bin.destroy(self);
-    self.* = std.mem.zeroes(Self);
 
     // Initialize registers.
     {
@@ -116,6 +121,9 @@ pub fn initMmio(base: usize) Error!*Self {
     log.debug("xHC version              : 0x{X}", .{self.capability.read(regs.CapInfo).hci_version});
     log.debug("xHC max slots            : {}", .{self.capability.read(regs.StructureParam1).maxslots});
     log.debug("xHC max ports            : {}", .{self.capability.read(regs.StructureParam1).maxports});
+
+    // Initialize DCBAA.
+    self.dcbaa = try Dcbaa.init();
 
     return self;
 }
@@ -155,14 +163,51 @@ pub fn reset(self: *Self) Error!void {
 pub fn setup(self: *Self) Error!void {
     // Initialize rings.
     try self.initRings();
+    // Enable  interrupts.
+    try self.enableInterrupt();
+    // Set DCBAA pointer.
+    self.operational.write(regs.Dcbaap, self.dcbaa.dcbaap());
 
-    // TODO
+    {
+        const irs0 = self.getIrsAt(0);
+        log.debug("xHC Primary Interrupter Register Set:", .{});
+        log.debug("  ERSTSZ: 0x{X}", .{@as(u32, @bitCast(irs0.read(regs.Erstsz)))});
+        log.debug("  ERSTBA: 0x{X}", .{@as(u64, @bitCast(irs0.read(regs.Erstba)))});
+        log.debug("  ERDP:   0x{X}", .{@as(u64, @bitCast(irs0.read(regs.Erdp)))});
+    }
+}
+
+/// Start the controller.
+pub fn run(self: *Self) void {
+    self.operational.modify(regs.CommandRegister, .{
+        .rs = true,
+    });
+
+    self.operational.waitFor(regs.StatusRegister, .{
+        .hch = false,
+    }, null);
+}
+
+/// Scan all ports.
+pub fn scan(self: *Self) mem.Error!void {
+    const max_ports = self.capability.read(regs.StructureParam1).maxports;
+
+    for (0..max_ports) |i| {
+        const port = self.getPortRegAt(i);
+
+        if (!port.read(regs.PortSc).ccs) {
+            continue;
+        }
+
+        // TODO: initialize device.
+    }
 }
 
 // =============================================================
 // Internals
 // =============================================================
 
+/// Initialize Command Ring and Event Ring.
 fn initRings(self: *Self) Error!void {
     // Init Command Ring.
     self.cring = try rings.Ring.new(rings.trbs_per_page, mem.page);
@@ -182,6 +227,22 @@ fn initRings(self: *Self) Error!void {
     self.ering.init();
 }
 
+/// Enable interrupts for the primary Interrupter.
+fn enableInterrupt(self: *Self) Error!void {
+    const irs0 = self.getIrsAt(0);
+
+    irs0.modify(regs.Imod, .{
+        .imodi = 4000, // 250ns * 4000 = 1ms
+    });
+    irs0.modify(regs.Iman, .{
+        .ie = true,
+        .ip = true,
+    });
+    self.operational.modify(regs.CommandRegister, .{
+        .inte = true,
+    });
+}
+
 /// Get the address of Interrupter Register Set (IRS) at the given index.
 fn getIrsAt(self: *Self, index: usize) regs.Interrupter {
     const rt_size = 32;
@@ -189,6 +250,65 @@ fn getIrsAt(self: *Self, index: usize) regs.Interrupter {
     const addr = self.runtime.base + rt_size + index * irs_size;
     return .new(addr);
 }
+
+/// Get the Port Register at the given index.
+fn getPortRegAt(self: *Self, index: usize) regs.Port {
+    const pr_size = 16;
+    const base = self.operational.getMarkerAddress(.port_set) + index * pr_size;
+    return .new(base);
+}
+
+// =============================================================
+// Data structures
+// =============================================================
+
+/// Device Context Base Address Array.
+const Dcbaa = struct {
+    /// Virtual pointer to DCBAA.
+    _raw: *RawDcbaa,
+
+    const RawDcbaa = extern struct {
+        /// Physical pointers to device contexts.
+        entries: [std.math.maxInt(u8)]usize,
+
+        comptime {
+            urd.comptimeAssert(@sizeOf(@This()) == 2040, "Invalid DCBAA size: {d}", .{@sizeOf(@This())});
+        }
+    };
+
+    /// Get the physical address of the DCBAA.
+    pub fn dcbaap(self: *const Dcbaa) usize {
+        return mem.page.translateIntP(self._raw);
+    }
+
+    /// Initialize DCBAA at the given memory.
+    pub fn init() mem.Error!Dcbaa {
+        const page = try mem.page.allocPagesV(1);
+        const raw: *RawDcbaa = @ptrCast(page.ptr);
+        const storage: [*]u8 = @ptrCast(raw);
+
+        @memset(storage[0..@sizeOf(RawDcbaa)], 0);
+
+        return .{ ._raw = raw };
+    }
+
+    /// Deinitialize DCBAA.
+    pub fn deinit(self: *Dcbaa) void {
+        const ptr: [*]const u8 = @ptrCast(self._raw);
+        mem.page.freePages(ptr[0..mem.size_4kib]);
+    }
+
+    /// Set the Device Context for the given slot index.
+    pub fn set(self: *const Dcbaa, slot: u8, context: usize) void {
+        self._raw.entries[slot] = mem.page.translateIntV(context);
+    }
+
+    /// Get the pointer to the Device Context of the given slot index.
+    pub fn at(self: *const Dcbaa, slot: u8) ?usize {
+        const ret = self._raw.entries[slot];
+        return if (ret == 0) null else mem.page.translateIntV(ret);
+    }
+};
 
 // =============================================================
 // Imports
@@ -210,3 +330,4 @@ const regs = @import("registers.zig");
 const rings = @import("ring.zig");
 const trbs = @import("trb.zig");
 const Trb = trbs.Trb;
+const Device = @import("Device.zig");
