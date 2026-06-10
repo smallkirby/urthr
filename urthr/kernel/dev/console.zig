3ef4b665ae0d0bfff75d570fc2574284a769b5d2
smallkirby <ssmallkirby@gmail.com>
2026-06-10 17:39:14 +0900

    feat(dev): add ioctl operation to console device
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- urthr/kernel/dev/console.zig
+++ urthr/kernel/dev/console.zig
@@ -6,6 +6,7 @@ pub const fops = fs.File.Ops{
     .iterate = iterate,
     .read = read,
     .write = write,
+    .ioctl = ioctl,
     .close = close,
 };
 
@@ -25,7 +26,53 @@ fn iterate(_: *fs.File.Iterator, _: Allocator) fs.Error!?fs.File.IterResult {
     return null;
 }
 
+fn ioctl(_: *fs.File, request: u64, arg: usize) fs.Error!usize {
+    switch (@as(Request, @enumFromInt(request))) {
+        .tcgets => {
+            const ret: *urd.input.Termios = @ptrFromInt(arg);
+            ret.* = urd.input.getTermios();
+        },
+        .tcsets, .tcsetsw, .tcsetsf => {
+            const t: *const urd.input.Termios = @ptrFromInt(arg);
+            urd.input.setTermios(t.*);
+        },
+        .tiocgwinsz => {
+            const ret: *WinSize = @ptrFromInt(arg);
+            ret.* = .{
+                .row = 25,
+                .col = 80,
+                .xpixel = 0,
+                .ypixel = 0,
+            };
+        },
+        else => return fs.Error.Unsupported,
+    }
+    return 0;
+}
+
 fn close(_: *anyopaque, _: Allocator) void {}
+
+const WinSize = extern struct {
+    row: u16,
+    col: u16,
+    xpixel: u16,
+    ypixel: u16,
+};
+
+const Request = enum(u64) {
+    /// Get the current serial port settings.
+    tcgets = 0x5401,
+    /// Set the serial port settings.
+    tcsets = 0x5402,
+    /// Allow the output buffer to drain, and set the current serial port settings.
+    tcsetsw = 0x5403,
+    /// Allow the output buffers to drain, discard pending input, and set the current serial port settings.
+    tcsetsf = 0x5404,
+    /// Get window size.
+    tiocgwinsz = 0x5413,
+
+    _,
+};
 
 // =============================================================
 // Imports
