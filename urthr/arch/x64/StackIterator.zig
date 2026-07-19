78ef471936172015fd00dcdc833afb32212e0a7d
smallkirby <ssmallkirby@gmail.com>
2026-07-19 11:20:01 +0900

    feat(x64): init x64 STackIterator
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- urthr/arch/x64/StackIterator.zig
+++ urthr/arch/x64/StackIterator.zig
@@ -1,9 +1,39 @@
 pub const Self = @This();
 
-pub fn init(_: u64) Self {
-    @panic("unimplemented");
+fp: usize,
+
+pub fn init(start_fp: u64) Self {
+    return .{ .fp = start_fp };
 }
 
-pub fn next(_: *Self) ?u64 {
-    @panic("unimplemented");
+pub fn next(self: *Self) ?u64 {
+    const fp_addr = self.fp + 0;
+    const ra_addr = self.fp + @sizeOf(usize);
+
+    if (!isFrameValid(fp_addr, ra_addr)) {
+        return null;
+    }
+
+    const fp_ptr: *const usize = @ptrFromInt(fp_addr);
+    const ret_ptr: *const usize = @ptrFromInt(ra_addr);
+
+    const fp = fp_ptr.*;
+    const ra = ret_ptr.*;
+
+    self.fp = fp;
+    return ra;
 }
+
+fn isFrameValid(fp: usize, ra: usize) bool {
+    return fp != 0 and
+        ra != 0 and
+        std.mem.isAligned(fp, @sizeOf(usize)) and
+        std.mem.isAligned(ra, @sizeOf(usize)) and
+        true;
+}
+
+// =============================================================
+// Imports
+// =============================================================
+
+const std = @import("std");
