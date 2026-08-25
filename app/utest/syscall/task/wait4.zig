8711eac789032f20e704e3d545073306224e0a87
smallkirby <ssmallkirby@gmail.com>
2026-08-25 23:26:40 +0900

    test: use pipe to sync threads
    Signed-off-by: smallkirby <ssmallkirby@gmail.com>

--- app/utest/syscall/task/wait4.zig
+++ app/utest/syscall/task/wait4.zig
@@ -95,27 +95,18 @@ test "waiting on a non-leader thread's own tid fails with ECHILD" {
 
 test "a child forked by one thread can be wait-ed by another thread in the same group" {
     const S = struct {
-        var grandchild_pid = std.atomic.Value(i32).init(0);
-        var waiter_ready = std.atomic.Value(u32).init(0);
-        var wait_succeeded = std.atomic.Value(i32).init(-1);
+        fn waiter(fds: usize) callconv(.c) u8 {
+            const pfds: *[2]i32 = @ptrFromInt(fds);
 
-        // Waits for the grandchild's PID to be published, then wait on it.
-        fn waiter(_: usize) callconv(.c) u8 {
-            // Wait for the grandchild's PID to be published.
-            var spins: usize = 0;
-            var gc_pid: i32 = 0;
-            while (spins < 100_000) : (spins += 1) {
-                gc_pid = grandchild_pid.load(.acquire);
-                if (gc_pid != 0) break;
-                _ = linux.sched_yield();
-            }
+            // Read the grandchild's PID from the pipe.
+            var gc_pid: i32 = undefined;
+            _ = linux.read(pfds[0], std.mem.asBytes(&gc_pid), @sizeOf(i32));
 
-            // Wait on the grandchild's PID.
             // The grandchild is not forked by this thread though belongs to the same process.
-            waiter_ready.store(1, .release);
             var status: u32 = undefined;
             const ret = linux.wait4(gc_pid, &status, 0, null);
-            wait_succeeded.store(if (ret == @as(usize, @intCast(gc_pid))) 1 else 0, .release);
+            const ok = [_]u8{if (ret == @as(usize, @intCast(gc_pid))) 1 else 0};
+            _ = linux.write(pfds[1], &ok, 1);
             linux.exit(0);
         }
     };
@@ -130,10 +121,21 @@ test "a child forked by one thread can be wait-ed by another thread in the same 
     defer _ = linux.close(gc_gate[0]);
     defer _ = linux.close(gc_gate[1]);
 
+    var gc_pid_chan: [2]i32 = undefined;
+    try testing.expectEqual(.SUCCESS, linux.errno(linux.pipe2(&gc_pid_chan, .{})));
+    defer _ = linux.close(gc_pid_chan[0]);
+    defer _ = linux.close(gc_pid_chan[1]);
+
+    var result_chan: [2]i32 = undefined;
+    try testing.expectEqual(.SUCCESS, linux.errno(linux.pipe2(&result_chan, .{})));
+    defer _ = linux.close(result_chan[0]);
+    defer _ = linux.close(result_chan[1]);
+
     const ret = linux.fork();
     if (ret == 0) {
         // Spawn a thread that will wait on the grandchild's PID.
-        _ = utest.task.spawnThread(S.waiter, 0) catch linux.exit_group(2);
+        var wfds = [2]i32{ gc_pid_chan[0], result_chan[1] };
+        _ = utest.task.spawnThread(S.waiter, @intFromPtr(&wfds)) catch linux.exit_group(2);
 
         // Fork a grandchild that blocks until told to exit.
         const gcret = linux.fork();
@@ -142,31 +144,18 @@ test "a child forked by one thread can be wait-ed by another thread in the same 
             _ = linux.read(gc_gate[0], &gbuf, 1);
             linux.exit(42);
         }
-        S.grandchild_pid.store(@intCast(gcret), .release);
 
-        // Wait for the waiter thread to be ready to wait on the grandchild.
-        var spins: usize = 0;
-        while (S.waiter_ready.load(.acquire) == 0) : (spins += 1) {
-            if (spins > 100_000) break;
-            _ = linux.sched_yield();
-        }
-        // Give the waiter thread some opportunity to actually enter the blocking state.
-        for (0..100) |_| _ = linux.sched_yield();
+        // Tell the waiter the grandchild's PID.
+        const gc_pid: i32 = @intCast(gcret);
+        _ = linux.write(gc_pid_chan[1], std.mem.asBytes(&gc_pid), @sizeOf(i32));
 
         // Let the grandchild exit.
         const go = [_]u8{1};
         _ = linux.write(gc_gate[1], &go, 1);
 
-        // Wait for the waiter thread to report the reap result.
-        spins = 0;
-        while (S.wait_succeeded.load(.acquire) == -1) : (spins += 1) {
-            if (spins > 100_000) break;
-            _ = linux.sched_yield();
-        }
-
-        // Check wait result and report it to the parent.
-        const msg = [_]u8{if (S.wait_succeeded.load(.acquire) == 1) 1 else 0};
-        _ = linux.write(to_parent[1], &msg, 1);
+        var result: [1]u8 = undefined;
+        _ = linux.read(result_chan[0], &result, 1);
+        _ = linux.write(to_parent[1], &result, 1);
         linux.exit(0);
     }
 
@@ -178,8 +167,7 @@ test "a child forked by one thread can be wait-ed by another thread in the same 
     try testing.expectEqual(@as(usize, 1), linux.read(to_parent[0], &buf, 1));
     try testing.expectEqual(@as(u8, 1), buf[0]);
 
-    var status: u32 = undefined;
-    _ = linux.wait4(child_pid, &status, 0, null);
+    try utest.expectWaitChild(child_pid, 0);
 }
 
 // =============================================================
